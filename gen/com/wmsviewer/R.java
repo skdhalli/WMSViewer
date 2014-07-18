997757fcc7b7755850cd262fce30692ce34aac14
Sai Dhalli <saidhalli@essargeo.com>
2014-07-18 00:36:24 -0700

    UI changes

--- gen/com/wmsviewer/R.java
+++ gen/com/wmsviewer/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050047;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1105,18 +1105,23 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-        public static final int go_wms=0x7f05003e;
+        public static final int go_wms=0x7f05003f;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int img_view=0x7f050040;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int pandown=0x7f050046;
+        public static final int panleft=0x7f050043;
+        public static final int panright=0x7f050045;
+        public static final int panup=0x7f050044;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1140,11 +1145,13 @@ containing a value of this type.
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
+        public static final int url_section=0x7f05003c;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
-        public static final int wms_url=0x7f05003d;
-        public static final int wms_url_txt=0x7f05003c;
-        public static final int zoomin=0x7f05003f;
+        public static final int wms_url=0x7f05003e;
+        public static final int wms_url_txt=0x7f05003d;
+        public static final int zoomin=0x7f050041;
+        public static final int zoomout=0x7f050042;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
